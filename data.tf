cfac00e61303936ade5dffeb451d270910affd76
Valejo J <70662055+valejojohnson@users.noreply.github.com>
2024-01-21 17:23:11 -0800

    Create data.tf

---
+++ data.tf
@@ -0,0 +1,14 @@
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "subnets" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+data "aws_iam_role" "eks" {
+  name = "AWSServiceRoleForAmazonEKS"
+}
